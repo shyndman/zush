b8d53768ad179d4b08156afbdea58dd06c7f76ec
Scott Hyndman <scotty.hyndman@gmail.com>
2025-08-06 07:49:14 -0400

    Ensure that brew loads before other tools, for its PATH

--- rc.d/10-lazy-tools.zsh
+++ rc.d/10-lazy-tools.zsh
@@ -5,6 +5,9 @@
 #       These commands can take 50-200ms each and their output rarely changes.
 #       We should cache the eval output and provide zush_clear_eval_cache function.
 
+# Homebrew - IMPORTANT: This must run before other tools
+zush_lazy_load brew 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' brew
+
 # Node.js via nvm
 zush_lazy_load nvm 'source ~/.nvm/nvm.sh' nvm node npm npx
 
@@ -13,6 +16,3 @@ zush_lazy_load pyenv 'eval "$(pyenv init -)"' pyenv python pip
 
 # Rust via cargo
 zush_lazy_load cargo 'source ~/.cargo/env' cargo rustc rustup
-
-# Homebrew
-zush_lazy_load brew 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' brew
