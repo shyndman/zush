34282a3b4a4503fb4178041345219299ab622e8d
Scott Hyndman <scotty.hyndman@gmail.com>
2025-08-08 22:22:20 -0400

    Add a couple more tools to associate with nvm

--- rc.d/10-lazy-tools.zsh
+++ rc.d/10-lazy-tools.zsh
@@ -9,7 +9,7 @@
 zush_lazy_load brew 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' brew
 
 # Node.js via nvm
-zush_lazy_load nvm 'source /home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh' nvm node npm npx
+zush_lazy_load nvm 'source /home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh' nvm node npm npx yarn pnpm
 
 # Python via pyenv
 zush_lazy_load pyenv 'eval "$(pyenv init -)"' pyenv python pip
