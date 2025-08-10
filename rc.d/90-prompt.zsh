03c77f9ba352e561835f9a65aa64d044b997b228
Scott Hyndman <scotty.hyndman@gmail.com>
2025-08-09 23:44:15 -0400

    Add a starship config env variable

--- rc.d/90-prompt.zsh
+++ rc.d/90-prompt.zsh
@@ -6,19 +6,19 @@ if ! command -v starship >/dev/null 2>&1; then
     return 1
 fi
 
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+
 # Ensure starship config is available
-_starship_config_file="$HOME/.config/starship/starship.toml"
-if [[ ! -f "$_starship_config_file" ]]; then
+if [[ ! -f "$STARSHIP_CONFIG" ]]; then
     if ! command -v curl >/dev/null 2>&1; then
         zush_error "Cannot download starship config: curl not available"
         return 1
     fi
-    
+
     zush_debug "Installing starship config"
-    mkdir -p "${_starship_config_file:h}"
-    curl -sSL "https://gist.githubusercontent.com/shyndman/01c8e8bfc197cfe8c56f41ca195921d1/raw/2615d222b3b297938987cba40beb8a83cc3a8233/starship.toml" -o "$_starship_config_file"
+    mkdir -p "${STARSHIP_CONFIG:h}"
+    curl -sSL "https://gist.githubusercontent.com/shyndman/01c8e8bfc197cfe8c56f41ca195921d1/raw/2615d222b3b297938987cba40beb8a83cc3a8233/starship.toml" -o "$STARSHIP_CONFIG"
 fi
-unset _starship_config_file
 
 # Initialize Starship prompt
 eval "$(starship init zsh)"
