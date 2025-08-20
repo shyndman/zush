cb4bd905d52a7d1e22660c8ec28ca12959b896d6
Scott Hyndman <scotty.hyndman@gmail.com>
2025-08-20 02:56:28 -0400

    Check for tool existence before caching

--- rc.d/10-lazy-tools.zsh
+++ rc.d/10-lazy-tools.zsh
@@ -2,13 +2,17 @@
 # Sets up lazy loading with environment caching for nvm, pyenv, cargo, and homebrew
 
 # Homebrew - IMPORTANT: This must run before other tools
-zush_lazy_eval brew '/home/linuxbrew/.linuxbrew/bin/brew shellenv' brew
+[[ -x /home/linuxbrew/.linuxbrew/bin/brew ]] && \
+    zush_lazy_eval brew '/home/linuxbrew/.linuxbrew/bin/brew shellenv' brew
 
 # Node.js via nvm
-zush_lazy_load nvm 'source /home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh' nvm node npm npx yarn pnpm
+[[ -f /home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh ]] && \
+    zush_lazy_load nvm 'source /home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh' nvm node npm npx yarn pnpm
 
 # Python via pyenv
-zush_lazy_eval pyenv 'pyenv init -' pyenv python pip uv uvx 
+command -v pyenv >/dev/null 2>&1 && \
+    zush_lazy_eval pyenv 'pyenv init -' pyenv python pip uv uvx 
 
 # Rust via cargo
-zush_lazy_load cargo 'source ~/.cargo/env' cargo rustc rustup
+[[ -f ~/.cargo/env ]] && \
+    zush_lazy_load cargo 'source ~/.cargo/env' cargo rustc rustup
