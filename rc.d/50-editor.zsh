17e4adca853ffa7fc813d2128bb6877c9082540a
Scott Hyndman <scotty.hyndman@gmail.com>
2026-01-14 00:39:54 -0500

    Enable OpenCode Exa web search

--- rc.d/50-editor.zsh
+++ rc.d/50-editor.zsh
@@ -1,6 +1,9 @@
 # Editor configuration
 # Sets up editor preferences based on environment
 
+# Enable Exa-backed web search for OpenCode
+export OPENCODE_ENABLE_EXA=true
+
 # Use VS Code if local (with -w to wait), vim if remote
 if [[ -z "$SSH_CONNECTION" ]] && (( ${+commands[code]} )); then
     export EDITOR="code -w"
@@ -9,3 +12,4 @@ else
     export EDITOR="vim"
     export VISUAL="vim"
 fi
+
