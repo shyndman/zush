57ea35296268b14287371ef7bdf7d8d169e2e90e
Scott Hyndman <scotty.hyndman@gmail.com>
2026-02-02 18:09:19 -0500

    Organize aliases a bit

--- rc.d/80-misc-aliases.zsh
+++ rc.d/80-misc-aliases.zsh
@@ -5,9 +5,14 @@ alias md='mkdir -p'
 mdc() { mkdir -p "$1" && cd "$1"; }
 alias rm='trash-put'
 alias g='git'
+
+# 1Password CLI Run
 alias opr='op run --no-masking --env-file .env -- '
 
+# Kittens
 alias kssh='kitten ssh'
 alias icat="kitten icat"
+
+# Clipboard
 alias -g cbcopy='kitten clipboard'
 alias -g cbpaste='kitten clipboard --get'
