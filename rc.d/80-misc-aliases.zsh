cc4400ee54f3ff307d6ed83ab16c79c970ebabd9
Scott Hyndman <scotty.hyndman@gmail.com>
2026-02-02 18:09:44 -0500

    Add a fd alias to fd that ignores less

--- rc.d/80-misc-aliases.zsh
+++ rc.d/80-misc-aliases.zsh
@@ -1,6 +1,9 @@
 alias _='sudo'
 alias %='whence -p'
 
+# I hate that by default, fd can't find anything
+alias fd='fd --no-ignore-vcs'
+
 alias md='mkdir -p'
 mdc() { mkdir -p "$1" && cd "$1"; }
 alias rm='trash-put'
