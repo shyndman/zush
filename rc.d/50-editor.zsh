bb9e45443ce60db7c407bc4c8a94dd032fdc4284
Scott Hyndman <scotty.hyndman@gmail.com>
2026-02-08 10:29:02 -0500

    Remove the OpenCode EXA flag

--- rc.d/50-editor.zsh
+++ rc.d/50-editor.zsh
@@ -1,9 +1,6 @@
 # Editor configuration
 # Sets up editor preferences based on environment
 
-# Enable Exa-backed web search for OpenCode
-export OPENCODE_ENABLE_EXA=true
-
 # Use VS Code if local (with -w to wait), vim if remote
 if [[ -z "$SSH_CONNECTION" ]] && (( ${+commands[code]} )); then
     export EDITOR="code -w"
